9f4932ffeec0b0a45f6e5e3f77c214f43d053ba8
Eyuti272 <Eyuti272@gmail.com>
2025-10-30 23:44:23 +0300

    finish

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+
    base: '/Evangadi_forum_frontend/',
-  },
+
 });
